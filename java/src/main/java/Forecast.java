4079665aff2225062726d2ff94f5599a6be266ad
Martín <martinsilleda@gmail.com>
2018-06-12 11:11:02 +0200

    Final solution.

--- java/src/main/java/Forecast.java
+++ java/src/main/java/Forecast.java
@@ -12,7 +12,50 @@ import java.util.Date;
 public class Forecast {
     public static final int ONE_DAY = 1000 * 60 * 60 * 24 * 1;
 
-    private String cityID(String city) throws IOException {
+    public String predict(String city, Date day, boolean wind) throws IOException {
+        if (wind) {
+            return predictWindSpeed(city, day);
+        }
+        return predictWheater(city, day);
+    }
+
+    public String predictWindSpeed(String city, Date dayRequested) throws IOException {
+        if (dayRequested == null) {
+            dayRequested = new Date();
+        }
+
+
+        if (requestedDateFiveDaysAhead(dayRequested)) {
+            return  "";
+        }
+        // If there are predictions
+
+        JSONObject requestedDay = getPredictionFromAPI(city, dayRequested);
+
+
+        return requestedDay.get("wind_speed").toString();
+    }
+
+    public String predictWheater(String city, Date dayRequested) throws IOException {
+        // When date is not provided we look for the current prediction
+        if (dayRequested == null) {
+            dayRequested = new Date();
+        }
+
+
+        if (requestedDateFiveDaysAhead(dayRequested)) {
+            return  "";
+        }
+
+
+        JSONObject requestedDay = getPredictionFromAPI(city, dayRequested);
+
+        return requestedDay.get("weather_state_name").toString();
+    }
+
+
+
+    private String getCityIDFromAPI(String city) throws IOException {
         // Find the id of the city on metawheather
         HttpRequestFactory requestFactory
                 = new NetHttpTransport().createRequestFactory();
@@ -25,7 +68,7 @@ public class Forecast {
         return id;
     }
 
-    private JSONArray consolidatedWhetaher(String id) throws IOException {
+    private JSONArray getWheatherFromAPI(String id) throws IOException {
         // Find the predictions for the city
         HttpRequestFactory requestFactory = new NetHttpTransport().createRequestFactory();
         HttpRequest request = requestFactory.buildGetRequest(
@@ -37,7 +80,7 @@ public class Forecast {
     }
 
 
-    public boolean requestedDateSixDaysAhead(Date dateRequested) {
+    public boolean requestedDateFiveDaysAhead(Date dateRequested) {
         Date sixDaysAhead = new Date(new Date().getTime() + ONE_DAY * 5);
         if (sixDaysAhead.before(dateRequested)) {
             return true;
@@ -45,32 +88,23 @@ public class Forecast {
         return false;
     }
 
-    public String predict(String city, Date datetime, boolean wind) throws IOException {
-        // When date is not provided we look for the current prediction
-        if (datetime == null) {
-            datetime = new Date();
-        }
-        String format = new SimpleDateFormat("yyyy-MM-dd").format(datetime);
-
-        if (requestedDateSixDaysAhead(datetime)) {
-            throw new OutOfDate();
-        }
-        // If there are predictions
-
-        String id = cityID(city);
-        JSONArray results = consolidatedWhetaher(id);
-
+    public JSONObject extractRequestedDay(JSONArray results, Date day) {
+        String dayFormatted = new SimpleDateFormat("yyyy-MM-dd").format(day);
         for (int i = 0; i < results.length(); i++) {
-//            // When the date is the expected
-            if (format.equals(results.getJSONObject(i).get("applicable_date").toString())) {
-//                // If we have to return the wind information
-                if (wind) {
-                    return results.getJSONObject(i).get("wind_speed").toString();
-                } else {
-                    return results.getJSONObject(i).get("weather_state_name").toString();
-                }
+            // When the date is the expected
+            if (dayFormatted.equals(results.getJSONObject(i).get("applicable_date").toString())) {
+                return results.getJSONObject(i);
             }
         }
-        return "";
+        return new JSONObject();
     }
+
+
+
+    private JSONObject getPredictionFromAPI(String city, Date dayRequested) throws IOException {
+        String id = getCityIDFromAPI(city);
+        JSONArray results = getWheatherFromAPI(id);
+        return extractRequestedDay(results, dayRequested);
+    }
+
 }
